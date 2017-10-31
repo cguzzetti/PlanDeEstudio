fbdb81ec8aaf0ecf757e150bd6820b6dd5aa931b
cguzzetti <cguzzetti@itba.edu.ar>
2017-10-31 10:49:56 -0300

    agregue en el tester clase para las materias de primer año

--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,8 @@
+import BackEnd.Clase;
 import BackEnd.Cuatrimestre;
 import BackEnd.Materia;
+import BackEnd.Plan;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,6 +12,8 @@ public class Tester {
     {
         List<Cuatrimestre> cuatrimestresInformatica=new ArrayList<>();
 
+      final String MADERO="Madero";
+      final String SDT="Distrito Tecnológico";
 
         Cuatrimestre c1=new Cuatrimestre("C1");
         Cuatrimestre c2=new Cuatrimestre("C2");
@@ -18,15 +23,33 @@ public class Tester {
         Cuatrimestre c6=new Cuatrimestre("C6");
 
         //todas las materias son en madero menos dos que indican que son en PP. Hay qeu fijarse bien por clase igual.
-        Materia algebra=new Materia("Álgebra",9,c1);// Martes 09:00 - 13:00, Jueves 09:00 - 14:00
-        Materia analisis1=new Materia("Análisis Matemático I",6,c1);//Miércoles 08:00 - 11:00, Viernes 08:00 - 11:00
-        Materia introInfo=new Materia("Introducción a la Informática",3,c1);//Lunes 08:00 - 11:00
-        Materia metodologia=new Materia("Metodología del Aprendizaje",3,c1);//Lunes 14:00 - 17:00
-        Materia sistemasDeRep=new Materia("Sistemas de Representación",3,c1);//Miércoles 14:00 - 17:00
-        Materia analisis2=new Materia("Análisis Matemático II",6,c2);//Lunes 12:00 - 14:00, Martes 11:00 - 13:00, Jueves 13:00 - 15:00
-        Materia fisica1=new Materia("Física I",6,c2,);//Martes 08:00 - 10:00, Jueves 11:00 - 13:00, Viernes 08:00 - 10:00
-        Materia mateDiscreta=new Materia("Matemática Discreta",6,c2);//Lunes 08:00 - 11:00, Jueves 08:00 - 11:00
-        Materia pi=new Materia("Programación Imperativa",9,c2);//Martes 13:00 - 17:00, Miércoles 10:00 - 13:00, Viernes 10:00 - 12:00
+        Materia algebra=new Materia("Álgebra",9,c1);
+        algebra.agregarClase(new Clase("Martes",MADERO,9,13));
+        algebra.agregarClase(new Clase("Jueves",MADERO,9,14));
+        Materia analisis1=new Materia("Análisis Matemático I",6,c1);
+        analisis1.agregarClase(new Clase("Miercoles",MADERO,8,11));
+        analisis1.agregarClase(new Clase("Viernes",MADERO,8,11));
+        Materia introInfo=new Materia("Introducción a la Informática",3,c1);
+        introInfo.agregarClase(new Clase("Lunes",MADERO,8,11));
+        Materia metodologia=new Materia("Metodología del Aprendizaje",3,c1);
+        metodologia.agregarClase(new Clase("Lunes",MADERO,14,17));
+        Materia sistemasDeRep=new Materia("Sistemas de Representación",3,c1);
+        sistemasDeRep.agregarClase(new Clase("Miércoles",MADERO,14,17));
+        Materia analisis2=new Materia("Análisis Matemático II",6,c2);
+        analisis1.agregarClase(new Clase("Lunes",MADERO,12,14));
+        analisis1.agregarClase(new Clase("Martes",MADERO,11,13));
+        analisis1.agregarClase(new Clase("Jueves",MADERO,13,15));
+        Materia fisica1=new Materia("Física I",6,c2);
+        fisica1.agregarClase("Martes",MADERO,8,10);
+        fisica1.agregarClase("Jueves",MADERO,11,13);
+        fisica1.agregarClase("Viernes",MADERO,8,10);
+        Materia mateDiscreta=new Materia("Matemática Discreta",6,c2);
+        mateDiscreta.agregarClase(new Clase("Lunes",MADERO,8,11));
+        mateDiscreta.agregarClase(new Clase("Jueves",MADERO,8,11));
+        Materia pi=new Materia("Programación Imperativa",9,c2);
+        pi.agregarClase(new Clase("Martes",MADERO,13,17));
+        pi.agregarClase(new Clase("Miércoles",MADERO,10,13));
+        pi.agregarClase(new Clase("Viernes",MADERO,10,12));
         Materia xml=new Materia("Diseño y Porcesamiento de Documentos XML",3,c3);//
         Materia fisica2=new Materia("Física II",6,c3);//Lunes 13:00 - 15:00, Martes 17:00 - 19:00, Miércoles 13:00 - 15:00
         Materia poo=new Materia("Programación Orientada a Objetos",6,c3);//Lunes 16:00 - 18:00, Viernes 16:00 - 20:00
@@ -48,7 +71,7 @@ public class Tester {
 
         pi.agregarCorrelativa(introInfo);
         pi.agregarCorrelativa(algebra);
-        mateDiscreta..agregarCorrelativa(algebra);
+        mateDiscreta.agregarCorrelativa(algebra);
         analisis2.agregarCorrelativa(algebra);
         analisis2.agregarCorrelativa(analisis1);
         fisica1.agregarCorrelativa(analisis1);
@@ -84,6 +107,8 @@ public class Tester {
         cuatrimestresInformatica.add(c4);
         cuatrimestresInformatica.add(c5);
         cuatrimestresInformatica.add(c6);
+
+        Plan plan=
 
 
 
